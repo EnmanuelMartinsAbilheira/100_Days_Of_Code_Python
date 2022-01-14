8aa2d793feb185f2d59aca7095eb199f7906d645
Enmanuel Martins <eel2000martins@hotmail.com>
2022-01-14 18:01:03 +0000

    day 3 finish

---
+++ day1/5 variables start.py
@@ -0,0 +1,12 @@
+name = input("What is your name?")
+print(name)
+
+name ="jack"
+print(name)
+
+name = "Angela"
+print(name)
+
+name = input("what is your name?")
+length = len(name)
+print(length)
