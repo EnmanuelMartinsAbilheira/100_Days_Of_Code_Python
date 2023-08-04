d2b273b661d99ba1c6f9dabdfc98a0b9a330019d
EnmanuelMartins <eel2000martins@hotmail.com>
2023-08-04 15:02:52 +0100

    Create 1 control Flow with if , else and conditional operators.py

---
+++ 003 day3 Beginner - Control Flow and Logical Operators/1 control Flow with if , else and conditional operators.py
@@ -0,0 +1,8 @@
+height = int(input("what is yopur height in cm?"))
+
+if height >= 120:
+    print("you can ride the rollercoaster")
+else:
+    print("sorry, you have to grow before you can ride")
+
+    
