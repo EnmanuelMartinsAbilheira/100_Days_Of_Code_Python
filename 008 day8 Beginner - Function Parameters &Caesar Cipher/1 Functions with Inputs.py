521e9afbdf839277ee03a92dc681e4a0b46d52de
EnmanuelMartins <eel2000martins@hotmail.com>
2023-08-09 14:03:47 +0100

    day 8

---
+++ 008 day8 Beginner - Function Parameters &Caesar Cipher/1 Functions with Inputs.py
@@ -0,0 +1,18 @@
+#review
+#create a function  called greet()
+# write 3 print statements inside the function
+# call the greet() function and trun yourr code 
+
+def greet():
+    print("Hello")
+    print("How do you do jack bauer")
+    print("isnt the weather nice today ")
+greet()
+
+
+#function that allows for input
+
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How do you do jack bauer {name}")
+greet_with_name("Enmanuel")
