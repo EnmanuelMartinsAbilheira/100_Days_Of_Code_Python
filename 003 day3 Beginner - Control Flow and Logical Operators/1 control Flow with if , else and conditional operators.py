0759ed3662ed0df490a86318891446374767b295
EnmanuelMartins <eel2000martins@hotmail.com>
2023-08-23 11:42:08 +0100

    Revert "come back again"
    This reverts commit b2308386cc615b3e4eb29a73f8ced6c7c2766027.

--- 003 day3 Beginner - Control Flow and Logical Operators/1 control Flow with if , else and conditional operators.py
+++
@@ -1,8 +0,0 @@
-height = int(input("what is yopur height in cm?"))
-
-if height >= 120:
-    print("you can ride the rollercoaster")
-else:
-    print("sorry, you have to grow before you can ride")
-
-    
