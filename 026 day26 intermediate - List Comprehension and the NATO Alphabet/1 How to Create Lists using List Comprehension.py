f5ab0da88601566cf349fd59c38f0592545d5609
EnmanuelMartins <eel2000martins@hotmail.com>
2023-09-07 09:49:44 +0100

    day 26 start

---
+++ 026 day26 intermediate - List Comprehension and the NATO Alphabet/1 How to Create Lists using List Comprehension.py
@@ -0,0 +1,9 @@
+numbers = [1,2,3]
+new_numbers = [n + 1 for n in numbers]
+
+name = "Angela"
+letters_list = [letter for letter in name]
+
+range_list = [num * 2 for num in range(1,5)]
+
+names = ["Alex","Beth","Caroline","Dave","Elanor","Freddie"]
