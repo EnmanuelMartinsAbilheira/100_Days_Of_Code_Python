95d85dddc2ce2bfa7fdf4dee476d77d9073a869c
EnmanuelMartins <eel2000martins@hotmail.com>
2023-09-09 10:09:00 +0100

    [Interactive Coding Exercise] Dictionary Comprehension 2

---
+++ 026 day26 intermediate - List Comprehension and the NATO Alphabet/8 [Interactive Coding Exercise] Dictionary Comprehension 2.py
@@ -0,0 +1,38 @@
+weather_c = {
+    "Monday": 12,
+    "Tuesday": 14,
+    "Wednesday": 15,
+    "Thursday": 14,
+    "Friday": 21,
+    "Saturday": 22,
+    "Sunday": 24,
+}
+# 🚨 Don't change code above 👆
+
+
+# Write your code 👇 below:
+
+weather_f = {day:(temp_c*9/5) + 32 for (day, temp_c) in weather_c.items()}
+
+print(weather_f)
+
+
+
+
+### other format
+
+weather_c = {
+    "Monday": 12,
+    "Tuesday": 14,
+    "Wednesday": 15,
+    "Thursday": 14,
+    "Friday": 21,
+    "Saturday": 22,
+    "Sunday": 24,
+}
+
+weather_f = {day:weather_c[day] * 9/5 + 32 for day in weather_c}
+
+print(weather_f)
+
+
