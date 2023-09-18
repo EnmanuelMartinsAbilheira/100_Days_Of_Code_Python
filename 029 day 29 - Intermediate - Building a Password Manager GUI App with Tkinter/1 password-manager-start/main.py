33c22b39dd5beaaa8c6a1a924718d18f5bedbe47
EnmanuelMartins <eel2000martins@hotmail.com>
2023-09-18 14:06:09 +0100

    Challenge 3 - Saving Data to File

--- 029 day 29 - Intermediate - Building a Password Manager GUI App with Tkinter/1 password-manager-start/main.py
+++ 029 day 29 - Intermediate - Building a Password Manager GUI App with Tkinter/1 password-manager-start/main.py
@@ -1,9 +1,20 @@
 
 from tkinter import *
+import os
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
+
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+    with open("Password.txt", "a") as data_file:
+        data_file.write(f"{website} ! {email} ! {password} \n")
+        website_entry.delete(0, END)
+        password_entry.delete(0, END)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -31,9 +42,11 @@ password_label.grid(column=0, row=3)
 #Entries
 website_entry = Entry(width=35)
 website_entry.grid(row=1, column=1, columnspan=2)
+website_label.focus()
 
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1,  columnspan=2)
+email_entry.insert(0, "angela@gmail.com")
 
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
@@ -43,7 +56,7 @@ password_entry.grid(row=3, column=1)
 generate_password_buton = Button(text="Generate Password")
 generate_password_buton.grid(column=2, row=3)
 
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Add", width=36, command=save)
 add_button.grid(column=1, row=4,  columnspan=2)
 
 window.mainloop()
