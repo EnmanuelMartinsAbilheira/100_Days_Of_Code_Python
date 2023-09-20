822b288786cef32b8a883794baf9aeebcb477129
EnmanuelMartins <eel2000martins@hotmail.com>
2023-09-20 20:16:07 +0100

    Catching Exceptions: The try catch except finally Pattern

---
+++ 030 day 30 - Intermediate - Errors, Exceptions and JSON Data Improving the Password/1  Catching Exceptions The try catch except finally Pattern.py
@@ -0,0 +1,17 @@
+#FileNotFound
+# with open("a_file.txt") as file:
+#    file.read()
+
+
+#KeyError
+# a_dictionary = {"key": "value"}
+# value = a_dictionary["non_existent_key"]
+
+# IndexError
+#fruit_list = ["Apple","banana","pear"]
+#fruit = fruit_list[3]
+
+#TypeError
+#text = "abc"
+#print(text + 5)
+
