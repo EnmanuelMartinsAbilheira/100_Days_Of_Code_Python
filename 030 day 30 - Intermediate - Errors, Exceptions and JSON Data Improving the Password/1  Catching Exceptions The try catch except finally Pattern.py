c0979da7b30b5d36cb6dbddf85f33173e5d34b23
EnmanuelMartins <eel2000martins@hotmail.com>
2023-09-22 02:43:37 +0100

    Catching Exceptions: The try catch except finally Pattern

--- 030 day 30 - Intermediate - Errors, Exceptions and JSON Data Improving the Password/1  Catching Exceptions The try catch except finally Pattern.py
+++ 030 day 30 - Intermediate - Errors, Exceptions and JSON Data Improving the Password/1  Catching Exceptions The try catch except finally Pattern.py
@@ -22,6 +22,12 @@ except FileNotFoundError:
     file.write("something,")
 except KeyError as error_message:
     print(f"the key {error_message} does not exist ")
+else:
+    content = file.read()
+    print(content)
+finally:
+    file.close()
+    print("File was closed.")
 
 
 #KeyError
