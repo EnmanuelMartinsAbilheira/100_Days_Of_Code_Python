917feea1788e5148bfe450ee645bba2bbbc87093
EnmanuelMartins <eel2000martins@hotmail.com>
2023-09-22 23:39:51 +0100

    Raising your own Exceptions

--- 030 day 30 - Intermediate - Errors, Exceptions and JSON Data Improving the Password/1  Catching Exceptions The try catch except finally Pattern.py
+++ 030 day 30 - Intermediate - Errors, Exceptions and JSON Data Improving the Password/1  Catching Exceptions The try catch except finally Pattern.py
@@ -12,23 +12,32 @@
 #    file.write("something,")
 
 
-try:
-    file = open("a_file.txt")
-    a_dictionary = {"key": "value"}
-    print(a_dictionary["sadasd"])
-except FileNotFoundError:
-    print("There was an error")
-    file = open("a_file.txt", "w")
-    file.write("something,")
-except KeyError as error_message:
-    print(f"the key {error_message} does not exist ")
-else:
-    content = file.read()
-    print(content)
-finally:
-    file.close()
-    print("File was closed.")
+#try:
+#    file = open("a_file.txt")
+#    a_dictionary = {"key": "value"}
+#    print(a_dictionary["sadasd"])
+#except FileNotFoundError:
+#    print("There was an error")
+#    file = open("a_file.txt", "w")
+#    file.write("something,")
+#except KeyError as error_message:
+#    print(f"the key {error_message} does not exist ")
+#else:
+#    content = file.read()
+#    print(content)
+#finally:
+#    #file.close()
+#    #print("File was closed.")
+#    raise KeyError("This is an error that i made up.")
 
+height = float(input("height: "))
+weight = float(input("weight: "))
+
+if height > 3:
+    raise ValueError("Hum,an heigh should not be over 3 meters")
+
+bmi = weight / height **2
+print(bmi)
 
 #KeyError
 # a_dictionary = {"key": "value"}
