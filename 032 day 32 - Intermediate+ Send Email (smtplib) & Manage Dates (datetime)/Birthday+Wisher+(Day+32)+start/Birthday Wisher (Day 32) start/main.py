3b4cb648dd5aeb348db9dcded0c5371d5dd9fe41
EnmanuelMartins <eel2000martins@hotmail.com>
2023-10-01 10:02:33 +0100

    How to Send Emails with Python using SMTP

--- 032 day 32 - Intermediate+ Send Email (smtplib) & Manage Dates (datetime)/Birthday+Wisher+(Day+32)+start/Birthday Wisher (Day 32) start/main.py
+++ 032 day 32 - Intermediate+ Send Email (smtplib) & Manage Dates (datetime)/Birthday+Wisher+(Day+32)+start/Birthday Wisher (Day 32) start/main.py
@@ -29,7 +29,13 @@ smtplib.SMTP("smtp.gmail.com", port=587)
 my_email = "example@gmail.com"
 password = "1234AbCd"
 
-connection = smtplib.SMTP("smtp.gmail.com")
-connection.starttls()
-connection.login(user=my_email, password=password, to_addrs="addrees@gmail.com", msg="Hello")
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs="addrees@gmail.com",
+        msg="Subject:Hello\n\nThis is the body of my email."
+    )
+
 connection.close()
