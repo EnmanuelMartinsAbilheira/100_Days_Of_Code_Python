f2543972abd82b5f586ca3adb6704ca64ecae632
EnmanuelMartins <eel2000martins@hotmail.com>
2023-10-02 22:03:15 +0100

    Working with the datetime Module

--- 032 day 32 - Intermediate+ Send Email (smtplib) & Manage Dates (datetime)/Birthday+Wisher+(Day+32)+start/Birthday Wisher (Day 32) start/main.py
+++ 032 day 32 - Intermediate+ Send Email (smtplib) & Manage Dates (datetime)/Birthday+Wisher+(Day+32)+start/Birthday Wisher (Day 32) start/main.py
@@ -1,4 +1,4 @@
-import smtplib
+#import smtplib
 
 """
 A Note About the Next Lesson: Google SMTP Port
@@ -26,16 +26,25 @@ Use this App password in your Python code instead of your normal password.
 smtplib.SMTP("smtp.gmail.com", port=587)
 """
 
-my_email = "example@gmail.com"
-password = "1234AbCd"
+#my_email = "example@gmail.com"
+#password = "1234AbCd"
 
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(
-        from_addr=my_email,
-        to_addrs="addrees@gmail.com",
-        msg="Subject:Hello\n\nThis is the body of my email."
-    )
+#with smtplib.SMTP("smtp.gmail.com") as connection:
+#    connection.starttls()
+#    connection.login(user=my_email, password=password)
+#    connection.sendmail(
+#        from_addr=my_email,
+#        to_addrs="addrees@gmail.com",
+#        msg="Subject:Hello\n\nThis is the body of my email."
+#    )
 
-connection.close()
+#connection.close()
+
+import datetime as dt
+
+now = dt.datetime.now()
+year = now.year
+month = now.month
+day_of_week = now.weekday()
+
+date_of_birth = dt.datetime(year=1995, month=12, day=15)
