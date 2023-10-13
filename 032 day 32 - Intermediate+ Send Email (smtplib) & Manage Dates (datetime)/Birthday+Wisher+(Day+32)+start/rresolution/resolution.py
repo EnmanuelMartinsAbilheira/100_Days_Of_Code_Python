26af0028f03834c34667bda1ff465d0a36a500e0
EnmanuelMartins <eel2000martins@hotmail.com>
2023-10-14 00:02:47 +0100

    email and password

--- 032 day 32 - Intermediate+ Send Email (smtplib) & Manage Dates (datetime)/Birthday+Wisher+(Day+32)+start/rresolution/resolution.py
+++ 032 day 32 - Intermediate+ Send Email (smtplib) & Manage Dates (datetime)/Birthday+Wisher+(Day+32)+start/rresolution/resolution.py
@@ -3,15 +3,14 @@ import pandas
 import random
 import smtplib
 
-# 2. Check if today matches a birthday in the birthdays.csv
-# HINT 1: Create a tuple from today's month and day using datetime. e.g.
 today = datetime.now()
 today_tuple = (today.month, today.day)
 
-# HINT 2: Use pandas to read the birthdays.csv
+MY_EMAIL = ""
+MY_PASSWORD = ""
+
 data = pandas.read_csv("birthdays.csv")
 
-#Dictionary comprehension template for pandas DataFrame looks like this:
 birthdays_dict = {(data_row["month"], data_row["day"]):data_row  for (index, data_row) in data.iterrows()}
 
 if today_tuple in birthdays_dict:
@@ -19,7 +18,7 @@ if today_tuple in birthdays_dict:
     file_path = f"letter_templates/letter_{random.randint(1,3)}.txt"
     with open(file_path) as letter_file:
         contents = letter_file.read()
-        contents.replace("[NAME]", birthdays_person["name"])
+        contents = contents.replace("[NAME]", birthdays_person["name"])
 
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
